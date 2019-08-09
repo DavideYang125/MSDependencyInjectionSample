29775df6fd6166ce9617801d143750527eee065f
yangww <1483722265@qq.com>
2019-08-09 14:31:25 +0800

    microsoft dependency injection

---
+++ DependencyInjectionDemoConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjectionDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
